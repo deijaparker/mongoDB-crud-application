34ab0e8b2c9e9cc2b4209afb91656cdb80b0683d
deijaparker <deijaparker1@outlook.com>
2024-09-14 18:16:14 -0700

    Created user controller with CRUD operations: getUsers, createUser, updateUser, deleteUser

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const User = require("../models/User");
+
+// GET all users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// POST a new user
+exports.createUser = async (req, res) => {
+  const { name, email, age } = req.body;
+  const newUser = new User({ name, email, age });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// PUT update user
+exports.updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// DELETE user
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "User deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
